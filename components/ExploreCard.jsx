1a8706e9b460659e9fb1d6e70d70b175daf245a7
“Pradaap-SS” <pradaapss@gmail.com>
2023-06-08 12:09:08 -0500

    #12 added feedback and footer section

--- components/ExploreCard.jsx
+++ components/ExploreCard.jsx
@@ -25,7 +25,7 @@ const ExploreCard = ({ id, imgUrl, title, index, active, handleClick }) => (
     ) : (
       <div className="absolute bottom-0 p-8 flex justify-start w-full flex-col bg-[rgba(0,0,0,0.5)] rounded-b-[24px]">
         <div
-          className={`${styles.flexCenter} w-[60px] h-[60px] rounded-[24px] glassmorphism mb-[16px]`}
+          className={`${styles.flexCenter} w-[60px] h-[60px] rounded-[24px] glassmorphism mb-[14px]`}
         >
           <img
             src="/headset.svg"
